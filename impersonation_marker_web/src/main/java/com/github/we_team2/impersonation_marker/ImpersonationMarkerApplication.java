23732065a2c0be49d52b5eed066c3f3a11b1a449
DaisukeKURATA <d.kruata.38@gmail.com>
2019-03-09 18:18:41 +0900

    SpringBootアプリケーションを作成

---
+++ impersonation_marker_web/src/main/java/com/github/we_team2/impersonation_marker/ImpersonationMarkerApplication.java
@@ -0,0 +1,13 @@
+package com.github.we_team2.impersonation_marker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpersonationMarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpersonationMarkerApplication.class, args);
+	}
+
+}
---
+++ impersonation_marker_web/src/test/java/com/github/we_team2/impersonation_marker/ImpersonationMarkerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.we_team2.impersonation_marker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImpersonationMarkerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
