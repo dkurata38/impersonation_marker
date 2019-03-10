6fd400662a7243e2391097779310c4d2d8e4a435
DaisukeKURATA <d.kruata.38@gmail.com>
2019-03-10 13:18:35 +0900

    コメントを追加

--- impersonation_marker_web/src/main/java/com/github/we_team2/impersonation_marker/application/service/ImpersonationMarkingServiceImpl.java
+++ impersonation_marker_web/src/main/java/com/github/we_team2/impersonation_marker/application/service/ImpersonationMarkingServiceImpl.java
@@ -1,11 +1,15 @@
 package com.github.we_team2.impersonation_marker.application.service;
 
+import com.github.we_team2.impersonation_marker.application.client.ClassificationExecutor;
 import com.github.we_team2.impersonation_marker.domain.ClassificationResult;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ImpersonationMarkingServiceImpl implements ImpersonationMarkingService {
+
+    private ClassificationExecutor executor;
+
     /**
      * 得点を計算するサービス.
      * @param audio ユーザーから受け取った音声データ.
@@ -13,6 +17,14 @@ public class ImpersonationMarkingServiceImpl implements ImpersonationMarkingServ
      */
     @Override
     public ClassificationResult mark(MultipartFile audio) {
+
+        // Fileを保存するよ
+
+        // Pythonファイルを実行するよ
+
+        // 正解データにバインドするよ
+
+
         return null;
     }
 }
