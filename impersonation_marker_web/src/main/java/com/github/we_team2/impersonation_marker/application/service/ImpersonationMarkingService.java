0643fbb2d35ec10458c9e29713f5a72199f3122f
DaisukeKURATA <d.kruata.38@gmail.com>
2019-03-10 12:19:41 +0900

    サービスのメソッドシグネチャを変更 ファイルを受け取るようにしました。

--- impersonation_marker_web/src/main/java/com/github/we_team2/impersonation_marker/application/service/ImpersonationMarkingService.java
+++ impersonation_marker_web/src/main/java/com/github/we_team2/impersonation_marker/application/service/ImpersonationMarkingService.java
@@ -1,7 +1,7 @@
 package com.github.we_team2.impersonation_marker.application.service;
 
 import com.github.we_team2.impersonation_marker.domain.ClassificationResult;
-import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * ものまね音声の点数を計算するモデルを戻すサービス.
@@ -9,7 +9,8 @@ import org.springframework.stereotype.Service;
 public interface ImpersonationMarkingService {
     /**
      * 得点を計算するサービス.
+     * @param audio ユーザーから受け取った音声データ.
      * @return
      */
-    ClassificationResult mark();
+    ClassificationResult mark(MultipartFile audio);
 }
--- impersonation_marker_web/src/main/java/com/github/we_team2/impersonation_marker/application/service/ImpersonationMarkingServiceImpl.java
+++ impersonation_marker_web/src/main/java/com/github/we_team2/impersonation_marker/application/service/ImpersonationMarkingServiceImpl.java
@@ -2,16 +2,17 @@ package com.github.we_team2.impersonation_marker.application.service;
 
 import com.github.we_team2.impersonation_marker.domain.ClassificationResult;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ImpersonationMarkingServiceImpl implements ImpersonationMarkingService {
     /**
      * 得点を計算するサービス.
-     *
+     * @param audio ユーザーから受け取った音声データ.
      * @return
      */
     @Override
-    public ClassificationResult mark() {
+    public ClassificationResult mark(MultipartFile audio) {
         return null;
     }
 }
